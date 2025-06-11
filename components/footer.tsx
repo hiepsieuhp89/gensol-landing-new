d70c76902af4e8f3e5ce46359b9bf160d424fac8
tuna <tungcan2000@gmail.com>
2025-06-12 02:38:17 +0700

    update

--- components/footer.tsx
+++ components/footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
                         alt="GENSOL logo"
                         height={70}
                         width={70}
-                        className='object-cover filter scale-90 -ml-5'
+                        className='object-cover filter scale-90'
                         style={{
                            imageRendering: 'crisp-edges',
                         }}
